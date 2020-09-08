de3d9b184167fa41d142c326c0a3d3bdc44b93b9
TorenUK <td_fraser@hotmail.co.uk>
2020-09-08 15:22:45 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Body from "./components/Body";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div className="App">this is the app</div>;
+  return (
+    //BEM
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Body />
+            <Sidebar />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
