29b248e872aa546158fb4dfa1e2f6508d0e202bf
TorenUK <td_fraser@hotmail.co.uk>
2020-09-11 13:37:48 +0100

    test

--- src/components/GiftItem.js
+++ src/components/GiftItem.js
@@ -11,21 +11,21 @@ function GiftItem() {
       <div className="giftItem__slider">
         <Carousel>
           <div>
-            <img src="https://i.etsystatic.com/23541080/r/il/2afcc3/2406290305/il_794xN.2406290305_izon.jpg" />
+            <img />
           </div>
           <div>
-            <img src="https://i.etsystatic.com/23541080/r/il/ba188d/2406290309/il_794xN.2406290309_9enq.jpg" />
+            <img />
           </div>
           <div>
-            <img src="https://i.etsystatic.com/23541080/r/il/c12155/2358690686/il_794xN.2358690686_hqw8.jpg" />
+            <img />
           </div>
         </Carousel>
       </div>
       <div className="giftItem__text">
-        <h3>Seed Bomb Card</h3>
+        <h4></h4>
         <h3>£5.99</h3>
         <p>
-          <span> 15 </span> available{" "}
+          <span>15</span> available{" "}
         </p>
         <Button variant="outlined" size="medium">
           add to basket
