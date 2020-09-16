65c957ac0504d45db64029e640592b15818cb2fd
TorenUK <td_fraser@hotmail.co.uk>
2020-09-16 12:36:47 +0100

    now pulling product data from firebase

--- src/components/GiftItem.js
+++ src/components/GiftItem.js
@@ -5,28 +5,26 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Button from "@material-ui/core/Button";
 
-function GiftItem() {
+function GiftItem({ name, image1, image2, image3 }) {
   return (
     <div className="giftItem">
       <div className="giftItem__slider">
         <Carousel>
           <div>
-            <img />
+            <img src={image1} />
           </div>
           <div>
-            <img />
+            <img src={image2} />
           </div>
           <div>
-            <img />
+            <img src={image3} />
           </div>
         </Carousel>
       </div>
       <div className="giftItem__text">
-        <h4></h4>
+        <h4>{name}</h4>
         <h3>£5.99</h3>
-        <p>
-          <span>15</span> available{" "}
-        </p>
+
         <Button variant="outlined" size="medium">
           add to basket
         </Button>
--- src/components/Gifts.js
+++ src/components/Gifts.js
@@ -4,9 +4,30 @@ import GiftItem from "./GiftItem";
 import database from "./firebase";
 
 function Gifts() {
+  const [items, setItems] = useState([]);
+
+  //run everytime component loads:
+
+  useEffect(() => {
+    database
+      .collection("gifts")
+      .onSnapshot((snapshot) =>
+        setItems(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, []);
+
   return (
     <div id="gifts" className="gifts">
-      <div className="gifts__container"></div>
+      <div className="gifts__container">
+        {items.map(({ name, images }) => (
+          <GiftItem
+            name={name}
+            image1={images[0]}
+            image2={images[1]}
+            image3={images[2]}
+          />
+        ))}
+      </div>
     </div>
   );
 }
