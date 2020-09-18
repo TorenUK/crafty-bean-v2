50303780b2477b5638385966c6d9ac9b0960e3e7
TorenUK <td_fraser@hotmail.co.uk>
2020-09-18 22:13:56 +0100

    added grid/flex containers to info page

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,18 +5,28 @@ import Footer from "./Footer";
 function Info() {
   return (
     <div id="info" className="info">
-      <div className="info__section">
-        <div className="info__section__text">
-          <p></p>
-        </div>
-        <div className="info__section__text">
-          <p></p>
-        </div>
-        <div className="info__section__text">
-          <p></p>
-        </div>
+      <div className="info__top-row">
+        <div className="info__segment">1</div>
+        <div className="info__segment">2</div>
+        <div className="info__segment">3</div>
       </div>
-      <div className="info__section">dummy text</div>
+      <div className="info__middle-row">
+        <div className="info__segment">1</div>
+        <div className="info__segment">2</div>
+        <div className="info__segment">3</div>
+        <div className="info__segment">4</div>
+        <div className="info__segment">5</div>
+        <div className="info__segment">6</div>
+        <div className="info__segment">7</div>
+        <div className="info__segment">8</div>
+        <div className="info__segment">9</div>
+      </div>
+      <div className="info__bottom-row">
+        <div className="info__segment">1</div>
+        <div className="info__segment">2</div>
+        <div className="info__segment">3</div>
+      </div>
+
       <Footer />
     </div>
   );
