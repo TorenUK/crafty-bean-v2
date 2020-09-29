6d0f8c0070619040fba4495c251b5e6c452d2f97
TorenUK <td_fraser@hotmail.co.uk>
2020-09-29 21:32:20 +0100

    added scroll to top component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
+// components
 import Landing from "./components/Landing";
 import Gifts from "./components/Gifts";
 import Info from "./components/Info";
 import Basket from "./components/Basket";
 import Payment from "./components/Payment";
 import Orders from "./components/Orders";
+import ScrollToTop from "./components/ScrollToTop";
+
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements, useElements } from "@stripe/react-stripe-js";
 
@@ -15,35 +19,34 @@ const promise = loadStripe(
 );
 
 function App() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     //BEM
+
     <Router>
-      <div className="app">
-        <div className="app__body">
-          <Switch>
-            <Route path="/orders">
-              <Orders />
-            </Route>
-            <Route path="/payment">
-              <Elements stripe={promise}>
-                <Payment />
-              </Elements>
-            </Route>
-            <Route path="/basket">
-              <Basket />
-            </Route>
-            <Route path="/">
-              <Landing />
-              <Gifts />
-              <Info />
-            </Route>
-          </Switch>
+      <ScrollToTop>
+        <div className="app">
+          <div id="app__body" className="app__body">
+            <Switch>
+              <Route path="/orders">
+                <Orders />
+              </Route>
+              <Route path="/payment">
+                <Elements stripe={promise}>
+                  <Payment />
+                </Elements>
+              </Route>
+              <Route path="/basket">
+                <Basket />
+              </Route>
+              <Route path="/">
+                <Landing />
+                <Gifts />
+                <Info />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </ScrollToTop>
     </Router>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Landing.css";
 import Button from "@material-ui/core/Button";
 import FooterLinks from "./FooterLinks";
