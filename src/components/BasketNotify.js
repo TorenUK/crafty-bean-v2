df3b3e52c7a93064e62e1c85998531962ee14b9d
TorenUK <td_fraser@hotmail.co.uk>
2020-10-04 18:51:08 +0100

    added animation to basketNotify

--- src/components/BasketNotify.js
+++ src/components/BasketNotify.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./BasketNotify.css";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 import { useStateValue } from "./StateProvider";
 import { Link } from "react-router-dom";
+import { TweenMax, Power3 } from "gsap";
 
 function BasketNotify() {
   const [{ basket }] = useStateValue();
+  let basketNotify = useRef(null);
+
+  useEffect(() => {
+    TweenMax.fromTo(
+      basketNotify,
+      { opacity: 0, y: -200 },
+      { opacity: 1, y: 0, duration: 0.5 }
+    );
+  }, [basket]);
 
   return (
-    <div className="basketNotify">
+    <div ref={(el) => (basketNotify = el)} className="basketNotify">
       <Link to="/basket">
         <ShoppingBasketIcon />
       </Link>
